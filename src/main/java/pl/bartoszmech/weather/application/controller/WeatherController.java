d7167b99f696e1f0715c735576990866d62464f3
FuuKowatty <miedzio.2000@gmail.com>
2024-01-04 13:18:07 +0100

    feat: add query param and handle validation by implementing RequestValidator

--- src/main/java/pl/bartoszmech/weather/application/controller/WeatherController.java
+++ src/main/java/pl/bartoszmech/weather/application/controller/WeatherController.java
@@ -2,7 +2,10 @@ package pl.bartoszmech.weather.application.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.bartoszmech.weather.application.request.validator.RequestValidator;
+import pl.bartoszmech.weather.application.request.validator.ValidateRequestHandler;
 import pl.bartoszmech.weather.application.response.WeatherResponse;
 import pl.bartoszmech.weather.domain.weather.WeatherService;
 
@@ -13,7 +16,8 @@ public class WeatherController {
     WeatherService weatherService;
 
     @GetMapping( "/api/best-weather")
-    public WeatherResponse getWeather() {
+    public WeatherResponse getWeather(@RequestParam("date") String date) {
+        RequestValidator.validateDateFormat(date);
         return weatherService.getBestLocation();
     }
 }
