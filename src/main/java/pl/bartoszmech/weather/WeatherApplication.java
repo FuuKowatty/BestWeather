59d8acc10e4735cedf6b8d0ce94b897501e926f4
FuuKowatty <miedzio.2000@gmail.com>
2024-01-04 17:20:16 +0100

    feat: load dynamically properties from configuration

--- src/main/java/pl/bartoszmech/weather/WeatherApplication.java
+++ src/main/java/pl/bartoszmech/weather/WeatherApplication.java
@@ -2,8 +2,11 @@ package pl.bartoszmech.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.bartoszmech.weather.infrastructure.fetch.WeatherConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(WeatherConfigurationProperties.class)
 public class WeatherApplication {
 
 	public static void main(String[] args) {
